3ebcb0b23e085d7f4d0d4320e3ef764f37f78feb
mk-vzla <mic.gonzalezs@duocuc.cl>
2025-05-24 21:05:20 -0400

    Actualizar archivos ignorados según .gitignore

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,8 +7,33 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class LoginPage implements OnInit {
+  public alertaConexion = [
+    {
+      text: 'No',
+      cssClass: 'alert-button-cancel',
+      handler: () => {
+        this.noConectarseLogin();
+        return true;
+      }
+    },
+    {
+      text: 'Yes',
+      cssClass: 'alert-button-confirm',
+      handler: () => {
+        this.conectarseLogin();
+        return true;
+      }
+    },
+  ];
 
   constructor() { }
+
+  conectarseLogin() {
+    console.log('CONECTANDO, MOSTRANDO EN CONSOLA');
+  }
+  noConectarseLogin() {
+    console.log('NO CONECTANDO, MOSTRANDO EN CONSOLA');
+  }
 
   ngOnInit() {
   }
