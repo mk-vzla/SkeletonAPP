4e51b3e5e64953bc68dec8cdb8d450d92e68a794
mk-vzla <mic.gonzalezs@duocuc.cl>
2025-06-02 15:24:21 -0400

    doneEx2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -13,11 +14,13 @@ export class HomePage {
   today = new Date();
 
   email: string = '';
-
   nombre: string = '';
   apellido: string = '';
   nivelEducacion: string = '';
   fechaNacimiento: string = '';
+
+  animarNombre = false;
+  animarApellido = false;
 
   constructor(private router: Router, private alertController: AlertController) {
     const nav = this.router.getCurrentNavigation();
@@ -36,7 +39,28 @@ export class HomePage {
   }
 
   mostrar() {
-    this.mostrarAlerta('Su nombre es: ' + this.nombre + ' ' + this.apellido);
+    if (!this.nombre || !this.apellido) {
+      this.mostrarAlerta('Por favor, complete los campos de nombre y apellido.');
+      return;
+    }
+    const nombreInitCap = this.nombre.charAt(0).toUpperCase() + this.nombre.slice(1).toLowerCase();
+    const apellidoInitCap = this.apellido.charAt(0).toUpperCase() + this.apellido.slice(1).toLowerCase();
+    this.mostrarAlerta('Su nombre es: ' + nombreInitCap + ' ' + apellidoInitCap);
+  }
+
+  limpiar() {
+    this.nombre = '';
+    this.apellido = '';
+
+    // Activar animación
+    this.animarNombre = true;
+    this.animarApellido = true;
+
+    // Quitar animación después de 1 segundo (duración del shake)
+    setTimeout(() => {
+      this.animarNombre = false;
+      this.animarApellido = false;
+    }, 1000);
   }
 
 }
